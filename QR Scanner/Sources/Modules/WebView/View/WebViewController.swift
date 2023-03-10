f061f12bc8014713a2317f760117dafe26750b07
Artem Galay <iostema@mail.ru>
2023-03-10 16:31:23 +0300

    Add NetworkManager

--- QR Scanner/Sources/Modules/WebView/View/WebViewController.swift
+++ QR Scanner/Sources/Modules/WebView/View/WebViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 final class WebViewController: UIViewController {
 
+    var url = String()
     private let webView = WKWebView()
     private let progressView = UIProgressView(progressViewStyle: .default)
 
@@ -22,6 +23,7 @@ final class WebViewController: UIViewController {
 
     private func setupHierarchy() {
         view.addSubview(webView)
+        view.addSubview(progressView)
     }
 
     private func setupLayout() {
@@ -30,37 +32,76 @@ final class WebViewController: UIViewController {
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 
     private func configureUI() {
-//        let progressButton = UIBarButtonItem(customView: progressView)
-//        toolbarItems = [progressButton]
-//        navigationController?.isToolbarHidden = false
+        view.backgroundColor = .white
+        webView.backgroundColor = .white
+        let shareButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareInfo))
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(tappedBackButton))
+
         navigationItem.setHidesBackButton(true, animated: false)
-        navigationItem.titleView = progressView
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItem = shareButton
+        navigationController?.hidesBarsOnSwipe = true
+        
         progressView.trackTintColor = .white
+        progressView.translatesAutoresizingMaskIntoConstraints = false
 
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         webView.navigationDelegate = self
         webView.translatesAutoresizingMaskIntoConstraints = false
     }
 
-    func loadRequest(link: String) {
-        guard let url = URL(string: link) else { return }
-
+    func loadRequest() {
+        guard let url = URL(string: url) else { return }
         let urlRequest = URLRequest(url: url)
 
         webView.load(urlRequest)
+    }
+
+    @objc func shareInfo() {
+
+        NetworkManager.shared.fetchData(url: url) { result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async() {
+                    let activityViewController = UIActivityViewController(activityItems: [data], applicationActivities: nil)
+                    activityViewController.completionWithItemsHandler = { _, success, _, error in
+                        if success {
+                            let alert = UIAlertController(title: "Поздравляем!", message: "Ваш файл успешно загружен", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                        if error != nil {
+
+                        }
+                    }
+                    self.present(activityViewController, animated: true, completion: nil)
+                }
+
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    @objc func tappedBackButton() {
+        navigationController?.popViewController(animated: true)
     }
 }
 
 extension WebViewController: WKNavigationDelegate {
 
     override func observeValue(forKeyPath keyPath: String?,
-                                     of object: Any?,
-                                     change: [NSKeyValueChangeKey : Any]?,
-                                     context: UnsafeMutableRawPointer?) {
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
             progressView.progress = Float(webView.estimatedProgress)
         }
